4d98357dbf84f61a7230706d6775522615094087
Nimrod Bahar <nimrodbahar@gmail.com>
2016-06-21 18:23:03 +0200

    Added structure and style

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
 
-  has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
+  has_attached_file :image, styles: { medium: "700x500#", small: "350x250#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
